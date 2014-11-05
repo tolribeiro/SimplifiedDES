531b7788e15ebf930e8460a3096b9dfaf91a691a
tolribeiro <tor@ecomp.poli.br>
2014-11-05 17:27:55 -0300

    Structuring the main function

---
+++ sdes.c
@@ -0,0 +1,13 @@
+/*
+- Title: SDES - A simplified version of the DES Algorithm	 
+- Author: Thiago Ribeiro. 								 
+- Date: November 5th, 2014.								 
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+	return 0;
+}
