5a50ab47d10c0ad702d34cec8553cbb69dbdac85
tolribeiro <tor@ecomp.poli.br>
2014-11-06 15:00:56 -0300

    Separating functions into different files

---
+++ functions.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "headers.h"
+
+void xor(char *str1, char *str2, char *result)
+{
+	unsigned long int i;
+
+	if (strlen(str1) != strlen(str2)) {
+		printf("Error: xor operation does not support different string lengths.");
+		exit(0);
+	}
+
+	for (i = 0; i < strlen(str1); i++) {
+		if (str1[i] == str2[i]) {
+			result[i] = '0';
+		}
+		else result[i] = '1';
+	}
+	result[i] = '\0';
+}
