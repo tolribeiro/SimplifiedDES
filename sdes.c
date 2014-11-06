45678fdd9a94e51444ceae16496349f1cf0e2551
tolribeiro <tor@ecomp.poli.br>
2014-11-06 13:10:14 -0300

    Function that performs xor operation between two strings

--- sdes.c
+++ sdes.c
@@ -10,4 +10,22 @@ int main(int argc, char const *argv[])
 {
 
 	return 0;
+void xor(char *str1, char *str2)
+{
+	int i;
+
+	if (strlen(str1) != strlen(str2)) {
+		printf("Error: xor operation does not support different string lengths.");
+		return;
+	}
+
+	for (i = 0; i < strlen(str1); i++) {
+		if (str1[i] == str2[i]) {
+			result[i] = '0';
+		}
+		else result[i] = '1';
+	}
+	result[i] = '\0';
+
+	return;
 }
