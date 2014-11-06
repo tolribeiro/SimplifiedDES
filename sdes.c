7871bb376bfbdb3ec4ed3118591c15482bc4674e
tolribeiro <tor@ecomp.poli.br>
2014-11-06 15:01:46 -0300

    Version with functions in different files

--- sdes.c
+++ sdes.c
@@ -1,7 +1,8 @@
 /*
 - Title: SDES - A simplified version of the DES Algorithm	 
 - Author: Thiago Ribeiro. 								 
-- Date: November 5th, 2014.								 
+- Date: November 5th, 2014.					
+	Compiling: gcc -o sdes sdes.c functions.c -I.			 
 */
 
 #include <stdio.h>
@@ -10,43 +11,22 @@
 
 #define S_STRING 10
 
-char result[S_STRING+1]; //terminating character
+char result[S_STRING+1]; /* terminating character */
 char str1[S_STRING]; 
 char str2[S_STRING];
 
-int main(int argc, char const *argv[])
+int main()
 {
-	/*scanf("%s", str1);
+	scanf("%s", str1);
 	printf("%s\n", str1);
 
 	scanf("%s", str2);
 	printf("%s\n", str2);
 
 	printf("\n");
+	xor(str1, str2, result);
 
-	xor(str1, str2);
-	printf("%s", result);*/
+	printf("%s", result);
 
 	return 0;
-}
-
-//xor operation between two strings, same length
-void xor(char *str1, char *str2)
-{
-	int i;
-
-	if (strlen(str1) != strlen(str2)) {
-		printf("Error: xor operation does not support different string lengths.");
-		return;
-	}
-
-	for (i = 0; i < strlen(str1); i++) {
-		if (str1[i] == str2[i]) {
-			result[i] = '0';
-		}
-		else result[i] = '1';
-	}
-	result[i] = '\0';
-
-	return;
 }
