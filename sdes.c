d30b3680114540a11e6f4a9080efa77891c343ef
tolribeiro <tor@ecomp.poli.br>
2014-11-06 13:14:11 -0300

    First version that only tests xor function

--- sdes.c
+++ sdes.c
@@ -5,14 +5,32 @@
 */
 
 #include <stdio.h>
+#include <string.h>
+#include "headers.h"
+
+#define S_STRING 10
+
 char result[S_STRING+1]; //terminating character
 char str1[S_STRING]; 
 char str2[S_STRING];
 
 int main(int argc, char const *argv[])
 {
+	/*scanf("%s", str1);
+	printf("%s\n", str1);
+
+	scanf("%s", str2);
+	printf("%s\n", str2);
+
+	printf("\n");
+
+	xor(str1, str2);
+	printf("%s", result);*/
 
 	return 0;
+}
+
+//xor operation between two strings, same length
 void xor(char *str1, char *str2)
 {
 	int i;
