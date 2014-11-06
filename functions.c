078575010c82cebb9400891c0fe2f3d298c4ee85
tolribeiro <tor@ecomp.poli.br>
2014-11-06 20:49:05 -0300

    Initial permutation

--- functions.c
+++ functions.c
@@ -2,6 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void ip(char *result, char *str)
+{
+	result[0] = str[1];
+	result[1] = str[5];
+	result[2] = str[2];
+	result[3] = str[0];
+	result[4] =	str[3];
+	result[5] = str[7];
+	result[6] = str[4];
+	result[7] = str[6];
+	result[8] = '\0';
+}
 void xor(char *str1, char *str2, char *result)
 {
 	unsigned long int i;
