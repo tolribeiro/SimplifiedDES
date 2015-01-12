b8a9a687598d57892c18e4e7f7b8c6f21d34cd76
tolribeiro <tor@ecomp.poli.br>
2015-01-12 02:58:07 -0300

    New version of SDES, encryption only

--- sdes.c
+++ sdes.c
@@ -1,130 +1,121 @@
-/*
-- Title: SDES - A simplified version of the DES Algorithm	 
-- Author: Thiago Ribeiro. 								 
-- Date: November 5th, 2014.								 
-*/
+/*==============================================================================
+|           ---    Simplified Data Encryption Standard (SDES)    ---		   |
+|                                                                              |
+|  * A simplified version of the famous algorithm DES.                         |                                                
+|  * Author: Thiago Ribeiro		                                               |
+|  * January 2014						                                       |
+==============================================================================*/
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-void xor(char *str1, char *str2);
-void ip(char *result);
-void ip_inverse(char *str);
-void divide_block(char *left, char *right);
-void p10_key_permutation(char *key);
-void circular_left_shift(char *key);
-void p8_key_permutation(char *key);
-void key_generation(char *key);
-void expansion_permutation(char *str);
-void sbox_s0();
-void sbox_s1();
-int convert_to_decimal(char *str);
-void convert_to_binary(int num);
-void p4(char *str);
+char output[9];
+char key_tmp[11];
+char k1[9];
+char k2[9];
+char l[5];
+char r[5];
+char binary_tmp[3];
+char s0_box[4][4];
+char s1_box[4][4];
 
-char result[11]; // used for permutations: max is 10 (key) plus terminator character
-char plaintext[9]; // compatible for strcpy
-char left[5]; // 4 bits plus terminator character
-char right[5];
-char key[11]; // 10-bit size key plus terminator
-char key1[9]; // 8-bit subkey1 plus terminator character	
-char key2[9]; // 8-bit subkey2 plus terminator character
-char temp[11]; // temporary variable to generate subkey k2
-int s_0[4][4]; // s0 box
-char s_1[4][4]; // s1 box
-char tmp[3];
-int row, column;
-char S_str[3];
-char buffer[5];
-char p4_output[5];
-
-int main()
+const int IP[8] = 
 {
-	int i, tmp2, s0, s1;
+	1, 5, 2, 0, 3, 7, 4, 6
+};
 
-	//do {
-	//	scanf("%s", plaintext);
-	//} while (strlen(plaintext) != SIZE_BLOCK);
+const int IPI[8] = 
+{
+	3, 0, 2, 4, 6, 1, 7, 5
+};
 
-	//ip(plaintext);
-	//printf("%s\n", result);
-	//strcpy(plaintext, result);
- 	//divide_block(left, right);
- 	
- 	// key permutation test
- 	
- 	/*strcpy(key, "1010000010");
- 	p10_key_permutation(key);
-	//printf("%s\n", result);
-	circular_left_shift(result);
-	printf("%s\n", result);
-	strcpy(key, result);
-	p8_key_permutation(key);
-	printf("%s\n", result);
-	strcpy(key, "1010000010");
-	key_generation(key);
-	printf("%s\n", key1);
-	printf("%s\n", key2);*/
+const int EP[8] = 
+{
+	3, 0, 1, 2, 1, 2, 3, 0
+};
 
-	//strcpy(left, "1001");
-	strcpy(left, "1011");
-	expansion_permutation(left);
-	printf("%s\n", result);
-	strcpy(key, "10100010");
-	xor(result, key);
-	printf("%s\n", result);
+const int P4[4] =
+{
+	1, 3, 2, 0
+};
 
-	//sbox_s0
-	tmp[0] = result[0];
-	tmp[1] = result[3];
-	tmp[2] = '\0';
-	row = convert_to_decimal(tmp);
-	printf("%d\n", row);
-	tmp[0] = result[1];
-	tmp[1] = result[2];
-	tmp[2] = '\0';
-	column = convert_to_decimal(tmp);
-	printf("%d\n", column);
-	sbox_s0();
-	tmp2 = s_0[row][column];
-	printf("%d\n", tmp2);
-	s0 = tmp2;
-	//sbox_s1
-	tmp[0] = result[4];
-	tmp[1] = result[7];
-	tmp[2] = '\0';
-	row = convert_to_decimal(tmp);
-	printf("%d\n", row);
-	tmp[0] = result[5];
-	tmp[1] = result[6];
-	tmp[2] = '\0';
-	column = convert_to_decimal(tmp);
-	printf("%d\n", column);
-	sbox_s1();
-	tmp2 = s_1[row][column];
-	printf("%d\n", tmp2);
-	s1 = tmp2;
-	printf("%d %d\n", s0, s1);
-	convert_to_binary(s0);
-	strcpy(buffer, S_str);
-	convert_to_binary(s1);
-	strcat(buffer, S_str);
-	strcpy(p4_output, buffer);
-	printf("%s\n", p4_output);
-	p4(p4_output);
-	printf("%s\n", p4_output);
-	/*
-	S_str[0] = (char)(((int)'0')+s0);
-	S_str[1] = (char)(((int)'0')+s1);
-	S_str[2] = '\0';
-	printf("%c %c\n", S_str[0], S_str[1]);*/
-	//printf("%c, %c\n", result[1], result[2]);
-	
-	return 0;
+const int P10[11] = 
+{
+	2, 4, 1, 6, 3, 9, 0, 8, 7, 5
+};
+
+const int P8[8] =
+{
+	5, 2, 6, 3, 7, 4, 9, 8
+};
+
+const int SW[8] =
+{
+	4, 5, 6, 7, 0, 1, 2, 3
+};
+
+// ------------------------------------------------
+// ---		     Encryption Functions			---
+// ------------------------------------------------
+
+void initial_permutation(char *input)
+{
+	for (int i = 0; i < 8; i++) output[i] = input[IP[i]];
+	output[8] = '\0';
 }
 
+void initial_permutation_inverse(char *input)
+{
+	char output_final[9];
 
+	for (int i = 0; i < 8; i++) output_final[i] = input[IPI[i]];
+	output_final[8] = '\0';
+
+	strcpy(output, output_final);
+}
+
+void switch_halves(char *input)
+{
+	char out_tmp_switched[9];
+	
+	for (int i = 0; i < 8; i++) out_tmp_switched[i] = input[SW[i]];
+	out_tmp_switched[8] = '\0';	
+	
+	strcpy(output, out_tmp_switched);
+}
+
+void expansion_permutation(char *input)
+{
+	for (int i = 0; i < 8; i++) output[i] = input[EP[i]];
+	output[8] = '\0';
+}
+
+void p4_permutation(char *input)
+{
+	char F_output[5];
+
+	for (int i = 0; i < 4; i++) F_output[i] = input[P4[i]];
+	F_output[4] = '\0';	
+
+	strcpy(output, F_output);
+}
+
+void divide_block(char *output)
+{	
+	int i, j;
+
+	for (i = 0; i < 4; i++) {
+		l[i] = output[i];
+	}
+	l[i] = '\0';
+
+	for (j = 0; j < 4; j++) {
+		r[j] = output[i];
+		i++;
+	}
+	r[j] = '\0';
+}
 
 int convert_to_decimal(char *str)
 {
@@ -146,189 +137,21 @@ int convert_to_decimal(char *str)
 	return -1;
 }
 
-void convert_to_binary(int num)
+void convert_to_binary(int num, char *binary)
 {
-	if(num==0)
-	strcpy(S_str,"00");
+	if(num == 0)
+	strcpy(binary,"00");
 
-	if(num==1)
-	strcpy(S_str,"01");
+	if(num == 1)
+	strcpy(binary,"01");
 
-	if(num==2)
-	strcpy(S_str,"10");
+	if(num == 2)
+	strcpy(binary,"10");
 
-	if(num==3)
-	strcpy(S_str,"11");
+	if(num == 3)
+	strcpy(binary,"11");
 }
 
-
-void sbox_s0()
-{
-	int i, j, k = 0;
-	int item[16] = {1, 0, 3, 2,
-					3, 2, 1, 0,
-					0, 2, 1, 3,
-					3, 1, 3, 2};
-
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			s_0[i][j] = item[k];
-			k++;
-		}
-	}
-}
-
-void sbox_s1()
-{
-	int i, j, k = 0;
-	int item[16] = {0, 1, 2, 3,
-					2, 0, 1, 3,
-					3, 0, 1, 0,
-					2, 1, 0, 3};
-
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			s_1[i][j] = item[k];
-			k++;
-		}
-	}
-}
-
-void p4(char *str)
-{
-	p4_output[0] = buffer[1];
-	p4_output[1] = buffer[3];	
-	p4_output[2] = buffer[2];
-	p4_output[3] = buffer[0];
-	p4_output[4] = '\0';
-}
-
-void expansion_permutation(char *str)
-{
-	result[0] = str[3];
-	result[1] = str[0];
-	result[2] = str[1];
-	result[3] = str[2];
-	result[4] =	str[1];
-	result[5] = str[2];
-	result[6] = str[3];
-	result[7] = str[0];
-	result[8] = '\0';
-}
-
-void key_generation(char *key)
-{
-	// generating first subkey key1 from key
-	p10_key_permutation(key);
-	strcpy(key, result);
-	circular_left_shift(key);
-	strcpy(temp, key);
-	p8_key_permutation(key);
-	strcpy(key1, result);
-
-	// generating second subkey key2 from temp
-	circular_left_shift(temp);
-	circular_left_shift(temp);
-	p8_key_permutation(temp);
-	strcpy(key2, result);
-}
-
-void circular_left_shift(char *key)
-{
-	char temp = key[0];
-
-	for (int i = 0; i < 5; i++) {
-		key[i] = key[i+1];
-	}	
-	key[4] = temp;
-	temp = key[5];
-	for (int i = 5 ; i < 10; i++) {
-		key[i] = key[i+1];
-	}	
-	key[9] = temp;
-}
-
-void p8_key_permutation(char *key)
-{
-	result[0] = key[5];
-	result[1] = key[2];
-	result[2] = key[6];
-	result[3] = key[3];
-	result[4] =	key[7];
-	result[5] = key[4];
-	result[6] = key[9];
-	result[7] = key[8];
-	result[8] = '\0';
-}
-
-// key permutation
-void p10_key_permutation(char *key)
-{
-	result[0] = key[2];
-	result[1] = key[4];
-	result[2] = key[1];
-	result[3] = key[6];
-	result[4] =	key[3];
-	result[5] = key[9];
-	result[6] = key[0];
-	result[7] = key[8];
-	result[8] = key[7];
-	result[9] = key[5];
-	result[10] = '\0';
-}
-
-// divide the 8-bit block into 4-bit left and right
-void divide_block(char *left, char *right)
-{	
-	int i, j;
-
-	for (i = 0; i < 4; i++) {
-		left[i] = plaintext[i];
-	}
-	left[i] = '\0';
-
-	for (j = 0; j < 4; j++) {
-		right[j] = plaintext[i];
-		i++;
-	}
-	right[j] = '\0';
-}
-
-// initial permutation 
-void ip(char *str)
-{
-	result[0] = str[1];
-	result[1] = str[5];
-	result[2] = str[2];
-	result[3] = str[0];
-	result[4] =	str[3];
-	result[5] = str[7];
-	result[6] = str[4];
-	result[7] = str[6];
-	result[8] = '\0';
-}
-
-// inverse permutation 
-void ip_inverse(char *str)
-{
-	result[0] = str[3];
-	result[1] = str[0];
-	result[2] = str[2];
-	result[3] = str[4];
-	result[4] =	str[6];
-	result[5] = str[1];
-	result[6] = str[7];
-	result[7] = str[5];
-	result[8] = '\0';
-}
-
-// f_k function
-void fk(char left[], char right[])
-{
-
-}
-
-// xor implementation between two strings, same length
 void xor(char *str1, char *str2)
 {
 	unsigned long int i;
@@ -340,9 +163,200 @@ void xor(char *str1, char *str2)
 
 	for (i = 0; i < strlen(str1); i++) {
 		if (str1[i] == str2[i]) {
-			result[i] = '0';
+			output[i] = '0';
 		}
-		else result[i] = '1';
+		else output[i] = '1';
 	}
-	result[i] = '\0';
+	output[i] = '\0';
+}
+
+void s0_items()
+{
+	int i, j, k = 0;
+	int item[16] = {1, 0, 3, 2,
+					3, 2, 1, 0,
+					0, 2, 1, 3,
+					3, 1, 3, 2};
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			s0_box[i][j] = item[k];
+			k++;
+		}
+	}
+}
+
+void s1_items()
+{
+	int i, j, k = 0;
+	int item[16] = {0, 1, 2, 3,
+					2, 0, 1, 3,
+					3, 0, 1, 0,
+					2, 1, 0, 3};
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			s1_box[i][j] = item[k];
+			k++;
+		}
+	}
+}
+
+int sbox_s(char *output, int i)
+{
+	char tmp[3];
+	int row;
+	int column;
+	int s0;
+	int s1;
+
+	if (i == 0) {
+		//sbox_s0
+		tmp[0] = output[0];
+		tmp[1] = output[3];
+		tmp[2] = '\0';
+		row = convert_to_decimal(tmp);
+		tmp[0] = output[1];
+		tmp[1] = output[2];
+		tmp[2] = '\0';
+		column = convert_to_decimal(tmp);
+		s0_items();
+		s0 = s0_box[row][column];
+		return s0;
+	} else	if (i == 1) {
+		//sbox_s1
+		tmp[0] = output[4];
+		tmp[1] = output[7];
+		tmp[2] = '\0';
+		row = convert_to_decimal(tmp);
+		tmp[0] = output[5];
+		tmp[1] = output[6];
+		tmp[2] = '\0';
+		column = convert_to_decimal(tmp);
+		s1_items();
+		s1 = s1_box[row][column];
+		return s1;
+	} else return -1;
+}
+
+void F_mapping(char *output, char *subkey)
+{
+	char binary[3];
+	int s0;
+	int s1;
+	char output_sbox[5];
+
+	divide_block(output);
+	expansion_permutation(r);
+	xor(output, subkey);
+
+	s0 = sbox_s(output, 0);
+	convert_to_binary(s0, binary);
+	strcpy(output_sbox, binary);
+
+	s1 = sbox_s(output, 1);
+	convert_to_binary(s1, binary);
+	strcat(output_sbox, binary);
+
+	p4_permutation(output_sbox);
+}
+
+void fk(char *output, char *subkey)
+{
+	F_mapping(output, subkey);
+	xor(l, output);
+	strcat(output, r);
+}
+
+// ------------------------------------------------
+// ---		   Keys Generation Functions		---
+// ------------------------------------------------
+
+void p10_key_permutation(char *key)
+{
+	for (int i = 0; i < 10; i++) key_tmp[i] = key[P10[i]];
+	key_tmp[10] = '\0';
+}
+
+void circular_left_shift(char *key_tmp)
+{
+	char temp = key_tmp[0];
+
+	for (int i = 0; i < 5; i++) {
+		key_tmp[i] = key_tmp[i+1];
+	}	
+	key_tmp[4] = temp;
+	temp = key_tmp[5];
+	for (int i = 5 ; i < 10; i++) {
+		key_tmp[i] = key_tmp[i+1];
+	}	
+	key_tmp[9] = temp;
+}
+
+void p8_key_permutation(char *key_tmp, int k)
+{
+	if (k == 1) {
+		for (int i = 0; i < 8; i++) k1[i] = key_tmp[P8[i]];
+		k1[8] = '\0';
+	}
+	else if(k == 2) {
+		for (int i = 0; i < 8; i++) k2[i] = key_tmp[P8[i]];
+		k2[8] = '\0';	
+	} else return;
+}
+
+void keys_generation(char *key)
+{
+	// generating k1
+	p10_key_permutation(key);
+	circular_left_shift(key_tmp);
+	p8_key_permutation(key_tmp, 1);
+
+	// generating k2
+	circular_left_shift(key_tmp);
+	circular_left_shift(key_tmp);
+	p8_key_permutation(key_tmp, 2);
+}
+
+// ------------------------------------------------
+// ---            Encrypt Function   			---
+// ------------------------------------------------
+
+void encrypt(char *input, char *key)
+{
+	initial_permutation(input);
+	
+	fk(output, k1);
+	switch_halves(output);
+	fk(output, k2);
+	initial_permutation_inverse(output);
+}
+
+// ------------------------------------------------
+// ---              Main Function   			---
+// ------------------------------------------------
+
+int main(int argc, char const *argv[])
+{	
+	char input[9];
+	char key[11];
+	int opt;
+
+	do {
+		printf("\nPlaintext (8-bit): ");
+		scanf("%s", input);
+	} while (strlen(input) != 8);	
+
+	do {
+		printf("Key (10-bit): ");
+		scanf("%s", key);
+	} while (strlen(key) != 10);
+
+	keys_generation(key);
+	encrypt(input, key);
+	printf("Ciphertext (8-bit): %s\n", output);
+	
+	printf("\n");
+
+	return 0;
 }
